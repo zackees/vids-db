7b48a747b1e6936a7ac4ab72a43487add70453d1
zackees <z@zackees.com>
2022-05-04 18:40:11 -0400

    more tests

--- tests/test_full_text_search.py
+++ tests/test_full_text_search.py
@@ -23,6 +23,7 @@ class FullTextSearchDbTester(unittest.TestCase):
         shutil.rmtree(self.tempdir)
 
     def test(self) -> None:
+        """Test the full text search database."""
         db = FullTextSearchDb(index_path=self.tempdir)
         vid = VideoInfo(
             channel_name="RedPill78",
@@ -45,10 +46,53 @@ class FullTextSearchDbTester(unittest.TestCase):
         out = db.title_search("Red Pill")
         self.assertEqual(1, len(out))
 
-        out = db.title_search("Red Pill published:a week ago")
+        out = db.title_search("Red Pill date:a week ago")
         self.assertEqual(0, len(out))
 
-        out = db.title_search("Red Pill published:today")
+        out = db.title_search("Red Pill date:today")
+        self.assertEqual(1, len(out))
+
+    def test_double_add(self) -> None:
+        """Tests that videos can be added twice but they will only exist once in the database."""
+        db = FullTextSearchDb(index_path=self.tempdir)
+        vid = VideoInfo(
+            channel_name="RedPill78",
+            title="TheRedPill",
+            date_published=datetime.now(),
+            date_lastupdated=datetime.now(),
+            channel_url="https://www.youtube.com/channel/UC-9-kyTW8ZkZNDHQJ6FgpwQ",
+            source="youtube",
+            url="https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+            duration="60",
+            description="A cool video",
+            img_src="https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+            iframe_src="https://www.youtube.com/embed/dQw4w9WgXcQ",
+            views=1,
+        )
+        db.add_videos([vid])
+        db.add_videos([vid])
+        out = db.title_search("Red")
+        self.assertEqual(1, len(out))
+
+    def test_double_add2(self) -> None:
+        """Tests that the videos can be added in the same batch and only exist once in the database."""
+        db = FullTextSearchDb(index_path=self.tempdir)
+        vid = VideoInfo(
+            channel_name="RedPill78",
+            title="TheRedPill",
+            date_published=datetime.now(),
+            date_lastupdated=datetime.now(),
+            channel_url="https://www.youtube.com/channel/UC-9-kyTW8ZkZNDHQJ6FgpwQ",
+            source="youtube",
+            url="https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+            duration="60",
+            description="A cool video",
+            img_src="https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+            iframe_src="https://www.youtube.com/embed/dQw4w9WgXcQ",
+            views=1,
+        )
+        db.add_videos([vid, vid])
+        out = db.title_search("Red")
         self.assertEqual(1, len(out))
 
 
--- vid_db/db_full_text_search.py
+++ vid_db/db_full_text_search.py
@@ -18,9 +18,25 @@ from vid_db.video_info import VideoInfo
 SCHEMA = fields.Schema(
     url=fields.ID(stored=True, unique=True, sortable=True),
     channel_name=fields.TEXT(stored=True, sortable=True),
-    published=fields.DATETIME(stored=True, sortable=True),
+    date=fields.DATETIME(stored=True, sortable=True),
     title=fields.TEXT(stored=True, analyzer=FancyAnalyzer()),
+    views=fields.NUMERIC(stored=True, sortable=True, bits=64),
 )
+
+
+def _filter_out_duplicate_videos(videos: List[VideoInfo]) -> List[VideoInfo]:
+    found_urls = set()
+    filtered_videos = []
+    for video in videos:
+        if video.url in found_urls:
+            continue
+        found_urls.add(video.url)
+        filtered_videos.append(video)
+    if len(filtered_videos) != len(videos):
+        print(
+            f"Filtered out {len(videos) - len(filtered_videos)} duplicate videos."
+        )
+    return filtered_videos
 
 
 class FullTextSearchDb:
@@ -36,18 +52,20 @@ class FullTextSearchDb:
 
     def add_videos(self, videos: List[VideoInfo]) -> None:
         """Add videos to the database."""
+        videos = _filter_out_duplicate_videos(videos)
         with self.index.writer() as writer:
             with writer.group():
                 for vid in videos:
                     published: datetime = vid.date_published
-                    writer.add_document(
+                    writer.update_document(
                         url=vid.url,
                         channel_name=u(vid.channel_name),
-                        published=published,
+                        date=published,
                         title=u(vid.title),
+                        views=vid.views,
                     )
 
-    def title_search(self, query_string: str) -> List[Any]:
+    def title_search(self, query_string: str, limit: int = 40) -> List[Any]:
         """Searcher for videos by title."""
         qparser = QueryParser("title", schema=SCHEMA)
         qparser.add_plugin(DateParserPlugin(free=False))
@@ -57,7 +75,8 @@ class FullTextSearchDb:
             assert matcher.__class__.__name__ in [
                 "W3LeafMatcher",
                 "IntersectionMatcher",
+                "MultiMatcher",
             ], f"{matcher.__class__.__name__} was unexpected"
-            results = searcher.search(query, mask=None)
+            results = searcher.search(query, mask=None, limit=limit)
             urls = sorted([fields["url"] for fields in results])
             return urls
