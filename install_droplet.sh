9751fc0cd48576ed13ec48c75630e0880eb27050
zackees <z@zackees.com>
2022-05-07 19:31:21 -0400

    adds more installation steps

--- install_droplet.sh
+++ install_droplet.sh
@@ -7,3 +7,4 @@ sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt install python3.10 -y
 sudo apt install python3-pip -y
+sudo apt install python-is-python3 -y
