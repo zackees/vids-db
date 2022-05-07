7dda22160c59093b1a323f2269df3bceda94749f
zackees <z@zackees.com>
2022-05-07 19:27:46 -0400

    adds more installation steps

--- install_droplet.sh
+++ install_droplet.sh
@@ -2,3 +2,8 @@ sudo apt upgrade -y
 sudo apt-get install nginx -y
 sudo service nginx start
 sudo systemctl enable nginx
+
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install python3.10
+sudo apt install python3-pip
